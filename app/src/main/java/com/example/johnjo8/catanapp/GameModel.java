9cf22e10a8e957c82be0d559540f29a43568be52
JC Weber <jcweber10@gmail.com>
2017-06-05 17:52:01 -0400

    Added constructor for GameModel

--- app/src/main/java/com/example/johnjo8/catanapp/GameModel.java
+++ app/src/main/java/com/example/johnjo8/catanapp/GameModel.java
@@ -1,9 +1,32 @@
 package com.example.johnjo8.catanapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by johnjo8 on 6/5/2017.
  */
 
 public class GameModel  {
+    static int redDie, yellowDie,rollTotal,numPlayers,numRound;
+    static int[] numStats;
+    static String stats;
+    static List<String> turnStrings;
+
+    public GameModel(){
+        numStats = new int[11];
+        for(int i=0;i<numStats.length;i++){
+            numStats[i]=0;
+        }
+        turnStrings = new ArrayList<String>();
+        populateStats();
+    }
+
+    private void populateStats(){
+        stats="";
+        for(int i=0;i<numStats.length;i++){
+            stats+= (i+2)+": " + numStats[i];
+        }
+    }
 
 }
