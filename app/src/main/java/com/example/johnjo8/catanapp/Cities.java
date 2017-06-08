118b3402a7929f05b83519b8309e6796036fd145
DanielWeber97 <daniel.weber.443@gmail.com>
2017-06-08 01:30:34 -0400

    added red die

---
+++ app/src/main/java/com/example/johnjo8/catanapp/Cities.java
@@ -0,0 +1,38 @@
+package com.example.johnjo8.catanapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by dan97w on 6/7/2017.
+ */
+
+public class Cities extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cities_activity);
+    }
+
+    public void roll(View v) {
+
+    }
+
+    public void launchStats(View v) {
+
+    }
+
+    public void endGame(View v) {
+
+    }
+
+    public void launchAlchemist(View v){
+
+    }
+
+    public void launchRollsLog(View v){
+
+    }
+
+}
