6e22bf5f51fb6f052c0c26d31f6fb26a17fd8812
DanielWeber97 <daniel.weber.443@gmail.com>
2017-06-13 17:57:59 -0400

    added Game of Thrones Theme

--- app/src/main/java/com/example/johnjo8/catanapp/MainActivity.java
+++ app/src/main/java/com/example/johnjo8/catanapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.johnjo8.catanapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.provider.MediaStore;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,11 +11,14 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends Activity {
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.gottheme);
+        mediaPlayer.start();
         ((RadioButton) findViewById(R.id.twoP)).setChecked(true);
         GameModel model = new GameModel();
         GameModel.setNumPlayers(2);
@@ -29,12 +33,14 @@ public class MainActivity extends Activity {
 
     public void launchBase(View v){
         Log.v("TAG", "swapped new activity");
+        mediaPlayer.stop();
         Intent i = new Intent(this,Base.class);
         startActivity(i);
     }
 
     public void launchCities(View v){
         Log.v("TAG", "swapped new activity");
+        mediaPlayer.stop();
         Intent i = new Intent(this,Cities.class);
         startActivity(i);
     }
